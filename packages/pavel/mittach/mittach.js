be8fb91343a792a48548788069a45890600c0767
Daniel Rosowski <rosowski@smartsquare.de>
2024-02-08 11:29:55 +0100

    Initial commit

---
+++ packages/pavel/mittach/mittach.js
@@ -0,0 +1,48 @@
+const axios = require("axios")
+const { format, getDay } = require("date-fns")
+const { de } = require("date-fns/locale")
+const cheerio = require("cheerio")
+
+function formatDateToGerman(date) {
+  return format(date, "EEEE, d. MMMM yyyy", { locale: de })
+}
+
+async function getDishesForDay(url, datestring) {
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+  const tableRows = $(`h2:contains("${datestring}")`)
+    .parent()
+    .find("table > tbody > tr");
+  const gericht1 = tableRows.first();
+  const gericht2 = gericht1.next();
+  const gericht3 = gericht2.next();
+
+  const rows = [gericht1, gericht2, gericht3]
+
+  const dishes = []
+  rows.forEach((row) => {
+    const textContents = [];
+    row.find("td").each((i, element) => {
+      const innerHtml = $(element).html()
+      const text = innerHtml.replace(/<strong>(.*?)<\/strong>/g, '$1').replace(/<br\s*\/?>/g, ', ').trim()
+      textContents.push(text)
+    });
+    const dishText = textContents.join(": ");
+    dishes.push(dishText);
+  });
+  return dishes.join("\n");
+}
+
+const cafeUrl = "https://www.cafe-kriemelmann.de/speiseplan/"
+const today = new Date()
+const datestring = formatDateToGerman(today)
+
+function main(args) {
+  getDishesForDay(cafeUrl, datestring).then((dishes) => {
+    console.log(dishes)
+    return { "body": dishes }
+  });
+}
+
+exports.main = main
+main()
